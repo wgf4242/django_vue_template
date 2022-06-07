34585d052c67035166956bce256cfb3fb03fb49c
wgf4242 <wgf4242@gmail.com>
2022-06-07 14:55:53 +0800

    sync

--- client/src/api/authenticate_services.ts
+++ client/src/api/authenticate_services.ts
@@ -1,12 +1,16 @@
 import axios from 'axios'
+import {useProjectStore} from "@/stores/project";
 
+let store = useProjectStore();
 class AuthenticationService {
     url = '/api/token/'
+    url_current_user = '/api/current_user/'
     access = ''
 
     constructor() {
         const currentUser = JSON.parse(localStorage.getItem('currentUser') as any);
         this.access = currentUser && currentUser.access;
+        store.user = currentUser;
     }
 
     login(username:string, password:string) {
@@ -17,6 +21,7 @@ class AuthenticationService {
             if (access) {
                 this.access = access
                 localStorage.setItem('currentUser', JSON.stringify(res.data));
+                this.getCurrentUser();
                 return res
             } else {
                 return res
@@ -34,6 +39,15 @@ class AuthenticationService {
     update_password(old_password: string, new_password: string) {
         const update_url = '/update_password/'
         return axios.put(update_url, {old_password, new_password})
+    }
+    getCurrentUser() {
+        axios.get(this.url_current_user).then(res => {
+            let userInfo = res.data;
+            let item = JSON.parse(localStorage.getItem('currentUser') as any);
+            const r = {...item, ...res.data};
+            localStorage.setItem('currentUser', JSON.stringify(r));
+            store.user = userInfo;
+        });
     }
 }
 export const authenticationService = new AuthenticationService();
---
+++ client/src/stores/project.ts
@@ -0,0 +1,25 @@
+import {defineStore} from 'pinia'
+
+// type Foo = {
+//     project: any,
+//     username: string;
+// }
+export const useProjectStore = defineStore('project', {
+    state: () => {
+        return {project: {}, team: {}, user:{} } as any;
+    },
+    // could also be defined as
+    // state: () => ({ count: 0 })
+    actions: {
+        increment() {
+        },
+    },
+    getters: {
+        // userDesc: (state) => `${state.user.name}今年${state.user.age}岁了`,
+        //
+        // userBesidesDesc(): string { // 需注明类型
+        //     return `${this.user.age}岁的${this.user.name}` // 可以使用 this 获取值
+        // },
+
+    }
+})
