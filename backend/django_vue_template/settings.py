eeddae86c805d5fcb8ec4c7dcf69cb81d88820a9
wgf4242 <wgf4242@gmail.com>
2022-06-14 15:13:25 +0800

    sync

--- backend/django_vue_template/settings.py
+++ backend/django_vue_template/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.gzip.GZipMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
