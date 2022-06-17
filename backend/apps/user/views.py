98e3f3c68b585e91932a94f0dd80c2cf74223fc3
wgf4242 <wgf4242@gmail.com>
2022-06-17 09:11:59 +0800

    sync

--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+
 
 # Create your views here.
+@api_view(['GET'])
+def current_user(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
--- backend/django_vue_template/urls.py
+++ backend/django_vue_template/urls.py
@@ -20,10 +20,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
+from apps.user.views import current_user
+
 router = DefaultRouter()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'api/', include(router.urls)),
+    path('api/current_user/', current_user),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
