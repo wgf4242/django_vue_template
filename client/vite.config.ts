1175fc1acdfc19af4353a0ba87ee9fe94d6d8eee
wgf4242 <wgf4242@gmail.com>
2023-04-03 14:40:24 +0800

    sync

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
+      '/media': 'http://127.0.0.1:8000/',
       "/static": "http://127.0.0.1:8000/",
       "/api": {
         target: "http://127.0.0.1:8000/",
