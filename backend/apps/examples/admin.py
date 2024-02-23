719dc39cbe0aa2ed2235fd9ef7b96cc36058b359
wgf4242 <wgf4242@gmail.com>
2024-02-23 16:03:49 +0800

    sync

--- backend/apps/examples/admin.py
+++ backend/apps/examples/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
+
 from . import models
 
 admin.site.register(models.EmployeeExample)
-admin.site.register(models.Book)
+
+
+class BookResource(resources.ModelResource):
+    def get_export_headers(self):  # 通过 resource 自定义header
+        meta = self._meta.model._meta
+        headers = [field.verbose_name for field in meta.fields]
+        # from django.utils.encoding import force_str
+        # headers = [force_str(field.column_name) for field in self.get_export_fields()]
+        return headers
+
+    class Meta:
+        model = models.Book
+
+
+class BookAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
+    resource_class = BookResource
+
+
+admin.site.register(models.Book, BookAdmin)
--- backend/django_vue_template/settings.py
+++ backend/django_vue_template/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
     'rest_framework',
     'django_filters',
     'apps.user',
