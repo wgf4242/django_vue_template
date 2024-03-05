9f6f9b586cf991d22aa1d79b9c41bb914ff228b4
wgf4242 <wgf4242@gmail.com>
2024-03-06 07:26:00 +0800

    sync

---
+++ client/src/components/a02ChildComponent.vue
@@ -0,0 +1,21 @@
+<!--https://vuejs.org/api/sfc-script-setup.html#defineslots-->
+<!--https://blog.vuejs.org/posts/vue-3-3#typed-slots-with-defineslots-->
+
+<template>
+  <div>
+    <div v-for="item in props.data" :key="item.id">
+      <slot :name="item.slotName" :row="item" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" generic="T extends {id: number;slotName:string}">
+const props = defineProps<{
+  data: T[];
+}>();
+
+// defineSlots<{ default(props: { test: string }): any; }>();
+// defineSlots<{ bodyCell(props: { row: T; columnKey: string }): any; }>();
+</script>
+
+<style scoped></style>
